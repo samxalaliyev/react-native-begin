683bd28e10feedae131ab743bf1f5415233a9cf8
samxalaliyev <samxalaliyev19@gmail.com>
2026-06-11 00:26:53 +0400

    modal tutorial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Modal, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
 export default function App() {
+
+  const[visible, setVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+
+    <Button
+   title='Modal AC'
+   onPress={()=>setVisible(true)} 
+    />
+    <Modal
+    transparent
+    animationType='fade'
+    visible={visible}
+    // onRequestClose={()=>setVisible(false)}
+    >
+
+      <View style={styles.backDrop}>
+
+       <Button
+   title='Modal Bagla'
+   onPress={()=>setVisible(false)} 
+    />
+
+      </View>
+
+    </Modal>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    justifyContent: "center",
+    alignItems:"center"
   },
-});
+  backDrop:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#f90847'
+  }
+})
